2795fa752f285081ed5ff66c8dd486784ddbf3f0
Anthony Stolp <anthony.stolp@gmail.com>
2024-07-22 13:40:21 -0400

    added home sections data

--- index.js
+++ index.js
@@ -5,12 +5,17 @@ const app = express();
 const port = 5000;
 
 const navigationData = require("./navigation-data/navigation-data");
+const sectionData = require("./sections-data/sections-data");
 
 app.use(cors());
 app.use(express.json());
 
 app.get("/api/navigation", (req, res) => {
   res.json(navigationData);
+});
+
+app.get("/api/sections", (req, res) => {
+  res.json(sectionData);
 });
 
 app.listen(port, () => {
---
+++ sections-data/sections-data.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    key: "section1",
+    title: "Welcome to Backcountry Creative",
+    content:
+      "We craft stunning custom websites that reflect your unique brand and drive your business forward. With extensive experience in CMS platforms and bespoke coded solutions, we're ready to bring your vision to life.",
+  },
+  {
+    key: "section2",
+    title: "Web Development Expertise",
+    content:
+      "From responsive designs to complex web applications, our team delivers cutting-edge solutions tailored to your specific needs.",
+  },
+  {
+    key: "section3",
+    title: "Creative Services",
+    content:
+      "Beyond web development, we offer a range of creative services including branding, UI/UX design, and content creation to elevate your digital presence.",
+  },
+];
